d4169c060f410499c9d3eabff8eca9dda54f429c
nonPointer <604235688@qq.com>
2020-09-30 23:06:34 +0800

    feat: permutation

--- CBC.js
+++ CBC.js
@@ -13,6 +13,11 @@
 var input = 'Hello world!';
 var key = '';
 
+/**
+ * Covert ASCII string to binary string with 1 and 0
+ * @param str
+ * @returns {string}
+ */
 function str2bin(str) {
     let result = [];
     str.split('').forEach(element => {
@@ -21,6 +26,50 @@ function str2bin(str) {
         result.push(leading + bin);
     });
     return result.join('');
+}
+
+/**
+ * IP
+ * @param lst0
+ * @returns {*}
+ */
+function initialPermutation(lst0) {
+    let lst1 = lst0;
+    let positions = [
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7]
+    for (let i = 0; i < positions.length; ++i) {
+        lst1[i] = lst0[positions[i]];
+    }
+    return lst1;
+}
+
+/**
+ * IP^{-1}
+ * @param lst0
+ * @returns {*}
+ */
+function finalPermutation(lst0) {
+    let lst1 = lst0;
+    let positions = [
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25]
+    for (let i = 0; i < positions.length; ++i) {
+        lst1[i] = lst0[positions[i]];
+    }
+    return lst1;
 }
 
 // utf8 to base64
